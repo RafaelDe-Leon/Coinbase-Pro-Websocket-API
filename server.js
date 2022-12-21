1d1d5f9f2dc7fe138456b8d7d93bf61c81d919a2
RafaelDe-Leon <rafael.deleon12688@gmail.com>
2022-12-21 16:53:04 -0500

    got websocket connecting to coinbase working

--- server.js
+++ server.js
@@ -1,1 +1,56 @@
-// first commit
+const WebSocket = require('ws')
+
+// const httpserver = http.createServer((req, res) => {
+//   console.log('we have received a request')
+// })
+
+// Connect to coinbase URL
+const ws = new WebSocket('wss://ws-feed.exchange.coinbase.com')
+
+// When a client connects to the server
+// wss.on('connection', function connection(ws) {
+//   console.log('Client is connected')
+
+//   ws.on('open', function open() {
+//     ws.send({
+//       type: 'subscribe',
+//       product_ids: ['ETH-USD', 'ETH-EUR'],
+//       channels: [
+//         'level2',
+//         'heartbeat',
+//         {
+//           name: 'ticker',
+//           product_ids: ['ETH-BTC', 'ETH-USD'],
+//         },
+//       ],
+//     })
+//   })
+
+//   ws.on('message', function message(data) {
+//     console.log('received: ', data)
+//   })
+// })
+
+let message = {
+  type: 'subscribe',
+  product_ids: ['ETH-USD'],
+  channels: [
+    'level2',
+    'heartbeat',
+    {
+      name: 'ticker',
+      product_ids: ['ETH-BTC'],
+    },
+  ],
+}
+let jsonMsg = JSON.stringify(message)
+
+ws.on('open', function open() {
+  ws.send(jsonMsg)
+})
+
+ws.on('message', function message(data) {
+  console.log('received: %s', data)
+})
+
+// server.listen(8080)
